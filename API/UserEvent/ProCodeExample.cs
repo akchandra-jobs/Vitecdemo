44764ba5b289b4bcd900c3832be35ee9a9c0878b
akchandra <>
2024-08-21 19:15:57 +0530

    procode

---
+++ API/UserEvent/ProCodeExample.cs
@@ -0,0 +1,15 @@
+﻿namespace Vitecdemo.UserEvent
+{
+    public interface IProCodeExample
+    {
+        int ExampleMethod();
+    }
+
+    public class ProCodeExample: IProCodeExample
+    {
+        public int ExampleMethod()
+        {
+            return 2 * 10;
+        }
+    }
+}
